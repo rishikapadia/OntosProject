93ade1e8a23fad2e2901931da411258026d61875
Abhinava Singh <abhinavasingh16@gmail.com>
2014-09-06 16:27:49 -0700

    database stuff

--- ontosProject/ontosApp/models.py
+++ ontosProject/ontosApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    firstName = models.CharField(max_length=200)
+    lastName = models.CharField(max_length=200)
+    username = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    top10hits = []
+    metadata = []
+    def __unicode__(self):              # __unicode__ on Python 2
+        return self.firstName
+
+
+
