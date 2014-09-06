9d7f8b1500759313e61450ce4ed68ea08c93f920
Abhinava Singh <abhinavasingh16@gmail.com>
2014-09-06 16:53:25 -0700

    urls

--- ontosProject/ontosProject/urls.py
+++ ontosProject/ontosProject/urls.py
@@ -5,6 +5,12 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ontosProject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r"^$", ontosApp.views.index, name="evernote_index"),
+    url(r"^login/$", ontosApp.views.login, name="evernote_auth"),
+    url(r"^dashboard/$", ontosApp.views.dashboard, name="evernote_callback"),
+    url(r"^reset/$", "reset", name="evernote_auth_reset"),
+	url(r"^register/$", "register", name="register"),
 
     url(r'^admin/', include(admin.site.urls)),
+
 )
